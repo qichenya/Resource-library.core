2504b0d4ef9e1b05602a9b8372a92668cbcd0873
成成0v0 <chengcheng@miao.ms>
2025-03-14 08:12:06 +0800

    feat: 完成 Axum 的基本声明

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::*;
+
+mod handlers;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(handlers::root));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8352));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    info!("Listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
